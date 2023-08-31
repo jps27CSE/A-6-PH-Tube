5a6a4f7c6bd0eac6233e49c6a8634b25a80c4897
jps27CSE <jackpritomsoren@hotmail.com>
2023-08-31 19:11:08 +0600

    category section

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello");
+const fetchCatergories = async () => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/categories`
+  );
+  const data = await response.json();
+  categoriesButton(data.data);
+};
+
+const categoriesButton = (data) => {
+  const categoriesSection = document.getElementById("categories_section");
+
+  data.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <button class="btn mr-2">${category.category}</button>
+    `;
+
+    categoriesSection.appendChild(div);
+  });
+};
+
+fetchCatergories();
